99f0e82365fdf7d6c51124fb7d23bff090b37905
ISLAM-HANY-2022 <114772611+ISLAM-HANY-2022@users.noreply.github.com>
2022-12-17 15:52:50 +0200

    Add files via upload

---
+++ Prime_Numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Prime_Numbers
+{
+	public static class Program
+	{
+		public static void Main(string[]args)
+ 	    {
+ 	     Console.WriteLine("The First Number :");
+ 	     int n1 =int.Parse(Console.ReadLine());
+ 	     Console.WriteLine("The Last Number :");
+ 	     int n2 = int.Parse(Console.ReadLine());
+ 	     Console.WriteLine("___________\n");
+ 	     Console.WriteLine("The Prime Number :");
+ 	     int j;
+ 	     bool current = false;
+ 	     for(;n1 <= n2;n1++)
+ 	      {
+ 	     	for( j = 2; j < n1 ;j++)
+ 	     	{
+ 	          if(n1 % j==0)
+ 	          {
+ 	          current = true;
+ 	          }
+ 	        }
+ 	     	if(current==false)
+ 	     	{
+ 	     	 Console.WriteLine(""+j);	
+ 	     	}
+ 	     	else
+ 	     	current = false;
+ 	     	
+ 	   	  }
+     
+          
+        }
+	}
+}
